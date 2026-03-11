cdf10608fa6e0b4364848b7bc6bb2d11426f1660
Joel <joeltanxinwei@gmail.com>
2026-03-11 15:59:52 +0800

    power

--- src/power_init.c
+++ src/power_init.c
@@ -1,25 +1,22 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
 
-static const struct gpio_dt_spec mosfet =
-    GPIO_DT_SPEC_GET(DT_NODELABEL(gpio0), gpios);
-
-static const struct gpio_dt_spec led = {
-    .port = DEVICE_DT_GET(DT_NODELABEL(gpio0)),
-    .pin = 30,
-    .dt_flags = GPIO_ACTIVE_LOW,
-};
+#define AS5600_POWER_NODE DT_NODELABEL(as5600_power)
 
 static int power_init(void)
 {
-    if (!device_is_ready(led.port)) {
+    const struct gpio_dt_spec mosfet = GPIO_DT_SPEC_GET(AS5600_POWER_NODE, gpios);
+
+    if (!device_is_ready(mosfet.port)) {
         return 0;
     }
 
-    gpio_pin_configure(led.port, 30, GPIO_OUTPUT_ACTIVE);
-    gpio_pin_configure(led.port, 2, GPIO_OUTPUT_ACTIVE);
+    /* Drive MOSFET gate active (LOW) to power AS5600 */
+    gpio_pin_configure(mosfet.port, mosfet.pin, GPIO_OUTPUT_ACTIVE);
 
     return 0;
 }
 
+/* Run at application boot */
 SYS_INIT(power_init, APPLICATION, 0);
